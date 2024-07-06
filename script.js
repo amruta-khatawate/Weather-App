1e82933f6312d5957c66eb8b0e52e6065c1dfe3b
amruta khatawate <161815861+amruta-khatawate@users.noreply.github.com>
2024-07-06 21:47:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'b4dafe815cmsh6dcc424babd4c1ep11e9c9jsna573af5e4888',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
-};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}

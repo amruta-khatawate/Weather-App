50b25bb162f371585db80e73222f9ddd3f9049b5
amruta khatawate <amrutakhatawate206@gmail.com>
2024-07-10 22:35:11 +0530

    file updated

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ api_key = 'e520c3bc89f9ac64050f2437a75fb22f';
 
 function getCityCoordinates(){
       let cityName = cityInput.value.trim();
-      console.log(cityName);
+      cityInput.value = '';
+      if(!cityName) return;
+      let GEOCODING_API_URL = 'http://api.openweathermap.org/geo/1.0/direct?q={cityName},{state code},{country code}&limit={limit}&appid=${api_key}';
+      fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+            console.log(data);
+      }).catch(() => {
+            alert('Failed to fetch coordinates of $(cityName)');
+      });
 }
 
 searchBtn.addEventListener('click', getCityCoordinates);

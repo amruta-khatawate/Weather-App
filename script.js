b10b0e5fed30addca010cce0e5e135e34422bb3b
amruta khatawate <amrutakhatawate206@gmail.com>
2024-07-10 22:19:04 +0530

    file updated

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 let cityInput = document.getElementById('city-input'),
-searchBtn = document.getElementById('searchBtn')
+searchBtn = document.getElementById('searchBtn'),
+api_key = 'e520c3bc89f9ac64050f2437a75fb22f';
+
+searchBtn

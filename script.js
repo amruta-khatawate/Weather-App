bb9738389b1295fea3fbb1a0967d5b3aedaf8146
amruta khatawate <amrutakhatawate206@gmail.com>
2024-07-10 22:23:11 +0530

    file updated

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ let cityInput = document.getElementById('city-input'),
 searchBtn = document.getElementById('searchBtn'),
 api_key = 'e520c3bc89f9ac64050f2437a75fb22f';
 
-searchBtn
+function getCityCoordinates(){
+      let cityName = cityInput.value.trim();
+      console.log(cityName);
+}
+
+searchBtn.addEventListener('click', getCityCoordinates);

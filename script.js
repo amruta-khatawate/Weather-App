7be5103770c16794403a5b6ad8f759319bec9898
amruta khatawate <amrutakhatawate206@gmail.com>
2024-07-11 19:47:57 +0530

    file updated

--- script.js
+++ script.js
@@ -3,7 +3,37 @@ searchBtn = document.getElementById('searchBtn'),
 api_key = 'e520c3bc89f9ac64050f2437a75fb22f';
 
 function getWeatherDetails(name, lat, lon, country, state){
-      let FORECAST_API_URL = `api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&appid=${api_key}`
+      let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`,
+      WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`,
+      days = [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday'
+      ],
+      months = [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+      ];
+
+      fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
+            console.log(data);
+      }).catch(() => {
+            alert('Failed to fetch current weather');
+      });
 }
 
 
